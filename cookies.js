20c092e0b9214644afa88fbd8ccc1bef236a3464
Mateus <mateusboga@gmail.com>
2015-03-05 21:40:36 +0000

    cookies

--- cookies.js
+++ cookies.js
@@ -1,7 +1,32 @@
+//cookie function
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+
 //cookies
-document.cookie = "1='test1'; expires=Wed, 01 Jan 0202 00:00:00 GMT";
-document.cookie = "2='test2'; expires=Wed, 01 Jan 0202 00:00:00 GMT";
-document.cookie = "3='test3'; expires=Wed, 01 Jan 0202 00:00:00 GMT";
+
 
 
 
