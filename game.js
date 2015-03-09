e2e4f356c107d7643e0609b85496b78d3bb1392c
Mateus <mateusboga@gmail.com>
2015-03-09 17:40:21 +0000

    test

--- game.js
+++ game.js
@@ -6,8 +6,8 @@
 function show(id, value) {
     document.getElementById(id).style.display = value ? 'block' : 'none';
 };
- 
+
 window.onload = function() {
 	
-	onReady(function () { show('loading', false); }); 
+	show('loading', false);
 };
